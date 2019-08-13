6ba8927663d8c61560cf34dccd6978a17593798a
liuewei <liuwei3031@163.com>
2019-08-13 10:57:00 +0800

    添加工程到git

---
+++ test01.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: liuwei
+ * @Date: 2019/8/13 0013 10:34
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("main");
+        main(args);
+    }
+
+}
